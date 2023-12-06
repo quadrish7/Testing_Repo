1478c6c41b1a8b81015ffe53ddd349049bbbed49
Mohammed Sadrulhuda Quadri <mohammeds.q@ntsindia.co.in>
2023-12-06 10:50:35 +0530

    updated repo

--- releases.md
+++
@@ -1,6 +0,0 @@
-### Releases
-
-* __V 0.1.0__
-	* Features:
-		* Enabled Pureprofile tag on realtor.com
-	* Dev: Ram Mullapudi
--- src/vendors/shims.js
+++
@@ -1,8 +0,0 @@
-/*
-  IE Doesn't have a .startsWith either?
-*/
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function (str){
-    return this.lastIndexOf(str, 0) === 0;
-  };
-}
